046babde00a91a34488345004ac4c9de3d0b8fd4
Arvind Suryakant <arvind.suryakant@yoti.com>
2021-07-22 15:06:41 +0530

    added report script support in package.json

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -8,7 +8,7 @@ module.exports = class Page {
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open (path) {
-        browser.maximizeWindow();
+        //browser.maximizeWindow(); moved to config
         //browser.fullscreenWindow();
         return browser.url(`https://the-internet.herokuapp.com/${path}`)
     }
